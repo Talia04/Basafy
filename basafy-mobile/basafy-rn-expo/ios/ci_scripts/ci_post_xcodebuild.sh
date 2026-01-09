0e8eead4988d43c436e7e22e00c41c28f2f1bd2d
Talia04 <tanyachisepo04@gmail.com>
2026-01-09 00:52:40 -0500

    Avoid pipe errors in xcresult dump

--- basafy-mobile/basafy-rn-expo/ios/ci_scripts/ci_post_xcodebuild.sh
+++ basafy-mobile/basafy-rn-expo/ios/ci_scripts/ci_post_xcodebuild.sh
@@ -5,7 +5,7 @@ set -x
 RESULT_PATH="/Volumes/workspace/resultbundle.xcresult"
 
 if [[ -d "$RESULT_PATH" ]]; then
-  xcrun xcresulttool get --format json --path "$RESULT_PATH" | head -n 200
+  xcrun xcresulttool get --format json --path "$RESULT_PATH" | sed -n '1,200p'
 else
   echo "xcresult not found at $RESULT_PATH"
 fi
