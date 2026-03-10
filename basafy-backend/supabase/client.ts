ba2b40dea90cd3d963ab1fa01a9d9aecb7246907
Talia04 <tanyachisepo04@gmail.com>
2026-03-10 00:30:28 -0400

    chore: persist supabase auth session

--- basafy-backend/supabase/client.ts
+++ basafy-backend/supabase/client.ts
@@ -1,6 +1,7 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from './types/database';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
@@ -11,6 +12,7 @@ if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
 
 export const supabase = createClient<Database>(SUPABASE_URL ?? '', SUPABASE_ANON_KEY ?? '', {
   auth: {
+    storage: AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false
